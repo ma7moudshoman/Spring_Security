3ee397389e2253e27ad7f3cba98f337c4a82590c
ma7moudshoman <shomanm53@gmail.com>
2023-10-07 00:00:28 +0300

    first commit

---
+++ src/main/java/com/spring/springsecurity/MainController.java
@@ -0,0 +1,30 @@
+package com.spring.springsecurity;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("api/")
+public class MainController {
+
+    @GetMapping("main")
+    public String main(){
+        return "index";
+    }
+    @GetMapping("profile")
+    public String profile(){
+        return "profile/index";
+    }
+
+    @GetMapping("manage")
+    public String manage(){
+        return "management/index";
+    }
+    @GetMapping("admin")
+    public String admin(){
+        return "admin/index";
+    }
+
+
+}
---
+++ src/main/java/com/spring/springsecurity/loginController.java
@@ -0,0 +1,16 @@
+package com.spring.springsecurity;
+
+import org.hibernate.annotations.Cache;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("api/")
+public class loginController {
+
+    @GetMapping("login")
+    public String login(){
+        return "login";
+    }
+}
